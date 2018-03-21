c3b91c95916da35825580398ae0885d01fb49b88
胡志刚 <zhigang.hu@hand-china.com>
2018-03-21 16:50:15 +0800

    fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 module.exports = {
   entry: {
     reacts: ['react', 'react-dom', 'redux', 'react-redux', 'react-router-dom'],
-    rutils: ['redux-saga', 'redux-form', 'axios', 'classnames/bind', 'pure-render-decorator'],
+    rutils: ['redux-form', 'axios', 'classnames/bind', 'pure-render-decorator'],
     app: './src/main.js'
   },
   output: {
